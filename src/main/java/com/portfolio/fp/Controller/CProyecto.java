866d69ffaf1c53a91110d0ac374ba9ceb6143b7e
Facu-11 <fphatouros2@gmail.com>
2022-11-17 19:41:18 -0300

    back
    back1000

--- src/main/java/com/portfolio/fp/Controller/CProyecto.java
+++ src/main/java/com/portfolio/fp/Controller/CProyecto.java
@@ -26,7 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendport-bd5d3.web.app/")
+
 public class CProyecto {
     @Autowired
     SProyecto sProyecto;
